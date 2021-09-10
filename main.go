db94f0650f76b5a88fd57d151986e7af505beddc
dawnnnnnn <m1050596704@gmail.com>
2021-09-10 10:19:45 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"github.com/go-redis/redis"
+	"github.com/pelletier/go-toml/v2"
+	log "github.com/sirupsen/logrus"
+	"github.com/tidwall/gjson"
+	"golang.design/x/clipboard"
+	"io/ioutil"
+	"time"
+)
+
+type config struct {
+	RedisAddr     string
+	RedisPassword string
+	TopicKey      string
+	DeviceToken   string
+}
+
+var flag = false
+
+var RedisAddr = ""
+var RedisPassword = ""
+var DeviceToken = ""
+var TopicKey = ""
+
+func PublishRedis(client *redis.Client) {
+	ch := clipboard.Watch(context.TODO(), clipboard.FmtText)
+	for data := range ch {
+		if flag == false {
+			clipboardContent := string(data)
+			encodeString := base64.StdEncoding.EncodeToString([]byte(clipboardContent))
+			jsondata := make(map[string]string)
+			jsondata["type"] = "text"
+			jsondata["msg"] = encodeString
+			jsondata["uuid"] = DeviceToken
+			dataType, _ := json.Marshal(jsondata)
+			dataString := string(dataType)
+			client.Publish(TopicKey, dataString)
+		}
+
+	}
+}
+
+func SubscribeRedis(client *redis.Client) {
+	pubsub := client.Subscribe(TopicKey)
+	defer pubsub.Close()
+	for msg := range pubsub.Channel() {
+		message := gjson.Get(msg.Payload, "msg").String()
+		uuid := gjson.Get(msg.Payload, "uuid").String()
+		if uuid != DeviceToken {
+			decodeString, _ := base64.StdEncoding.DecodeString(message)
+			log.Infof("向剪切板写入信息: %s\n", decodeString)
+			clipboard.Write(clipboard.FmtText, decodeString)
+			flag = true
+			time.Sleep(time.Second * 1)
+			flag = false
+		}
+	}
+}
+
+func main() {
+	log.SetFormatter(&log.TextFormatter{
+		ForceQuote:      true,
+		TimestampFormat: "2006-01-02 15:04:05",
+		FullTimestamp:   true,
+	})
+
+	var Config config
+	bytes, err := ioutil.ReadFile("config.toml")
+	if err != nil {
+		panic(err)
+	}
+
+	err = toml.Unmarshal(bytes, &Config)
+
+	if err != nil {
+		panic(err)
+	}
+	RedisAddr = Config.RedisAddr
+	RedisPassword = Config.RedisPassword
+	DeviceToken = Config.DeviceToken
+	TopicKey = Config.TopicKey
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     RedisAddr,
+		Password: RedisPassword,
+	})
+
+	go SubscribeRedis(client)
+	PublishRedis(client)
+
+}
